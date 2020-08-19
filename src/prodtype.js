6009be8e81a9d9b73fca55f95a886f8258cd6c0b
Kevin Ashworth <kevinashworth@yahoo.com>
2020-08-19 09:06:18 -0700

    NMA - 5 new, 14 archived, 2 small edits

--- src/prodtype.js
+++ src/prodtype.js
@@ -23,7 +23,7 @@ const PRODTYPE_SELECTOR = 'select#edit-prodtype';
 const LISTINGS_AVAILABLE = '#production_listings_results #production_listings';
 const LISTINGS_SELECTOR = '#production_listings > [id^=row]';
 
-const PRODTYPE = 'TH';
+const PRODTYPE = 'NMA';
 const OUTPUT_DIR = `./output/prodtype/${PRODTYPE}`; // assumes we run `node src/prodtype.js`
 
 // reminder: runs in browser context
