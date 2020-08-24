f58aa66a7fe213d9dc5361373265add41adc413c
Kevin Ashworth <kevinashworth@yahoo.com>
2020-08-24 13:39:24 -0700

    LA - 19 new, 128 archived, 5 small edits

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  USER_AGENT: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
+  USER_AGENT: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36',
   WIDTH: 1440,
   HEIGHT: 766
 };
