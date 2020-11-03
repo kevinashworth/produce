d6ab83aad91d67e1ee381c3ac37b05734dc3137e
Kevin Ashworth <kevinashworth@yahoo.com>
2020-11-02 17:31:52 -0800

    Refactor prodtype
    Include all the stuff done recently to location refactor

--- src/prodtype.js
+++ src/prodtype.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const glob = require('glob');
 const difference = require('lodash/difference');
+const pull = require('lodash/pull');
 const remove = require('lodash/remove');
-const mkdirp = require('mkdirp');
+const homedir = require('os').homedir();
 const path = require('path');
-const puppeteer = require('puppeteer');
 
 const chalk = require('chalk');
 const error = chalk.bold.red;
@@ -12,80 +12,29 @@ const success = chalk.bold.green;
 const verbose = chalk.bold.yellow;
 
 const CONFIG = require('./config.js');
-const CONSTANTS = require('./constants.js');
-const CREDENTIALS = require('./credentials.js');
+const SELECTORS = require('./selectors.js');
 
-const PRODTYPE = 'TV';
-const OUTPUT_DIR = `./output/prodtype/${PRODTYPE}`; // assumes we run `node src/prodtype.js`
+const PRODTYPE = 'AG';
 
-// reminder: runs in browser context
-const handleListingsPageFn = (nodeListArray) => {
-  return nodeListArray.map(listing => {
-    const [id] = listing.id.match(/\d+/g);
-    return {
-      id,
-      production: listing.children[1].innerText,
-      type: listing.children[2].innerText,
-      local: listing.children[3].innerText,
-      startEndDates: listing.children[4].innerText
-    };
-  });
-};
+const OUTPUT_DIR = path.join(homedir, CONFIG.OUTPUT_DIR, 'prodtype', PRODTYPE);
+const OUTPUT_DIR_ARCHIVE = path.join(OUTPUT_DIR, 'archive');
+fs.mkdirSync(OUTPUT_DIR_ARCHIVE, { recursive: true });
+console.log('Output files will be in', OUTPUT_DIR);
 
-// reminder: runs in browser context
-const handleDetailsPageFn = (detailsElement) => {
-  const results = {};
-  const shootingLocations = [];
-  const alternateTitles = [];
-  const children = Array.from(detailsElement.children);
-  children.pop(); // last child is always an empty 'p' tag
-
-  for (let i = 0; i < children.length; i++) {
-    const child = children[i];
-    if (child.children[0]) {
-      // first, deal with two-parters here (shootingLocations, alternateTitles)
-      if (i < children.length - 1) {
-        const nextChild = children[i + 1];
-        const nextChildren = nextChild && nextChild.children && Array.from(nextChild.children);
-        if (nextChildren && nextChildren[0] && nextChildren[0].tagName === 'LI') {
-          if (child.innerText.indexOf('Shooting Locations') === 0) {
-            nextChildren.forEach(location => {
-              shootingLocations.push(location.innerText);
-            });
-            results.shootingLocations = shootingLocations;
-          } else if (child.innerText.indexOf('Alternate Titles') === 0) {
-            nextChildren.forEach(title => {
-              alternateTitles.push(title.innerText);
-            });
-            results.alternateTitles = alternateTitles;
-          }
-          // important next two lines
-          i++;
-          continue;
-        }
-      }
-      // else, deal with one-parters
-      const name = child.children[0].innerText.replace(/\W/g, '');
-      const nameKey = _.camelCase(name);
-      const nameFull = child.innerHTML;
-      const nameSpan = child.children[0].outerHTML;
-      const nameValue = nameFull.substring(nameSpan.length).trim();
-      results[nameKey] = nameValue;
-    }
-  }
-  return results;
-};
+const handlers = require('./handlers.js');
+const hooks = require('./hooks.js');
 
 (async () => {
-  const timeStart = new Date();
-  console.log('Start time:', timeStart);
+  const {
+    browser,
+    cookiesFile,
+    page,
+    timeStart
+  } = await hooks.before();
+
+  await hooks.beforeEach({ page, msg: `Begin (${PRODTYPE})` });
 
   let existingFiles = null;
-  mkdirp(OUTPUT_DIR + '/archive').then(made => {
-    if (made) {
-      console.log(verbose(`mkdirp ${made}`));
-    }
-  });
   glob(OUTPUT_DIR + '/*.json', null, function (err, files) {
     if (err) throw err;
     existingFiles = files.map(file => path.basename(file)).sort();
@@ -93,27 +42,12 @@ const handleDetailsPageFn = (detailsElement) => {
     console.log(existingFiles);
   });
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  console.log(verbose('Created page.'));
-  await page.setUserAgent(CONFIG.USER_AGENT);
-  await page.setViewport({
-    width: CONFIG.WIDTH,
-    height: CONFIG.HEIGHT
-  });
-  await page.goto(CONFIG.START_URL);
-  await page.type(CONSTANTS.USERNAME_SELECTOR, CREDENTIALS.username);
-  await page.type(CONSTANTS.PASSWORD_SELECTOR, CREDENTIALS.password);
-  await page.click(CONSTANTS.LOGIN_BUTTON_SELECTOR);
-  await page.waitForNavigation();
-  console.log(success('Logged in to Production Listings.'));
-  await page.addScriptTag({
-    url: 'https://cdn.jsdelivr.net/npm/lodash@4/lodash.min.js'
-  });
-  await page.select(CONSTANTS.PRODTYPE_SELECTOR, PRODTYPE);
-  await page.click(CONSTANTS.SEARCH_BUTTON);
-  await page.waitForSelector(CONSTANTS.LISTINGS_AVAILABLE);
-  const listings = await page.$$eval(CONSTANTS.LISTINGS_SELECTOR, handleListingsPageFn);
+  await page.select(SELECTORS.PRODTYPE_SELECTOR, PRODTYPE);
+  await page.select(SELECTORS.LOCATION_SELECTOR, 'ALL');
+  await page.click(SELECTORS.SEARCH_BUTTON);
+  await page.waitForSelector(SELECTORS.LISTINGS_AVAILABLE);
+
+  const listings = await page.$$eval(SELECTORS.LISTINGS_SELECTOR, handlers.handleListings);
   if (!listings) {
     console.log(error('No listings for', PRODTYPE));
   }
@@ -123,6 +57,7 @@ const handleDetailsPageFn = (detailsElement) => {
   console.log(verbose(`${listingsIds.length} listings on page:`));
   console.log(listingsIds);
   const toArchive = difference(existingFiles, listingsIds);
+  pull(toArchive, 'zero.json'); // (`pull` mutates `toArchive`)
   if (toArchive.length > 0) {
     console.log(verbose(`${toArchive.length} being moved to archive:`));
     console.log(toArchive);
@@ -131,7 +66,7 @@ const handleDetailsPageFn = (detailsElement) => {
       const fromPath = OUTPUT_DIR + '/' + file;
       const toPath = OUTPUT_DIR + '/archive/' + file;
       fs.renameSync(fromPath, toPath);
-      console.log('Moved %s to %s', fromPath, toPath);
+      console.log('Moved %s to %s', file, toPath);
     }
     // remove archive files before going thru listings (`remove` mutates `listings`)
     remove(listings, (listing) => {
@@ -141,6 +76,7 @@ const handleDetailsPageFn = (detailsElement) => {
     console.log(verbose('There are no files to archive.'));
   }
 
+  // scrape the listings
   console.log(verbose(`${listings.length} listings to process:`));
   for (let i = 0; i < listings.length; i++) {
     let listing = listings[i];
@@ -151,7 +87,6 @@ const handleDetailsPageFn = (detailsElement) => {
         if (err) throw err;
         console.log(verbose(outFile, 'got saved.'));
       });
-      await browser.close();
       console.log(success('End of program.'));
       return;
     }
@@ -160,23 +95,20 @@ const handleDetailsPageFn = (detailsElement) => {
     const detailsSelector = `#result-${id}.fulldetail.openDetail`;
     try {
       await page.click(clickSelector);
-    } catch (e) {
-      console.log(error(`page.click error, ${id}:`));
-      console.log(e);
-      console.log('The "for" loop will now continue.');
-      continue;
-    }
-    try {
       await page.waitForSelector(detailsAvailable);
     } catch (e) {
-      console.log(error(`page.waitForSelector error, ${id}:`));
+      console.log(error(`page.click or .waitForSelector error, ${id}:`));
       console.log(e);
       console.log('The "for" loop will now continue.');
       continue;
     }
     try {
-      const details = await page.$eval(detailsSelector, handleDetailsPageFn);
+      const details = await page.$eval(detailsSelector, handlers.handleDetails);
       listing = { ...listing, ...details };
+      // don't keep reps
+      delete listing.bgClaimsRepresentative;
+      delete listing.businessRepresentative;
+      delete listing.claimsRepresentative;
     } catch (e) {
       console.log(error(`page.$eval error, ${id}:`));
       console.log(e);
@@ -194,8 +126,8 @@ const handleDetailsPageFn = (detailsElement) => {
 
     // add details back in to listings array for writing to an all-in-one file
     listings[i] = listing;
-    const randomSeconds = Math.floor(Math.random() * 7000) + 3000; // 3 to 10 seconds
-    await page.waitFor(randomSeconds);
+    const randomSeconds = Math.floor(Math.random() * 5000) + 2000; // 2 to 7 seconds
+    await page.waitForTimeout(randomSeconds);
   }
 
   console.log(success('Finished with', listings.length, 'listings.'));
@@ -205,10 +137,12 @@ const handleDetailsPageFn = (detailsElement) => {
     console.log(success(outFile, 'has been saved.'));
   });
 
-  await page.waitFor(1500);
-  await browser.close();
-  const timeStop = new Date();
-  console.log('Stop time:', timeStop);
-  const duration = Math.floor((timeStop - timeStart) / 1000);
-  console.log(`Program took ${duration} seconds.`);
+  hooks.afterEach(`End (${PRODTYPE})`);
+
+  await hooks.after({
+    browser,
+    cookiesFile,
+    page,
+    timeStart
+  });
 })();
