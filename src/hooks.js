dd7b3e917f1fa440d19c17a566ea3c4e5d0d545a
Kevin Ashworth <kevinashworth@yahoo.com>
2020-11-02 17:30:12 -0800

    Refactor location
    Put before/after[Each] hooks in separate file

---
+++ src/hooks.js
@@ -0,0 +1,119 @@
+const fs = require('fs');
+const homedir = require('os').homedir();
+const path = require('path');
+const puppeteer = require('puppeteer');
+
+const chalk = require('chalk');
+const error = chalk.bold.red;
+const success = chalk.bold.green;
+const verbose = chalk.bold.yellow;
+
+const CREDENTIALS = require('./credentials.js');
+const CONFIG = require('./config.js');
+const PAGE_CONFIG = require('./page-config.js');
+const SELECTORS = require('./selectors.js');
+
+const before = async () => {
+  // 1 setup
+  const timeStart = new Date();
+  console.log('Start time:', timeStart.toLocaleString());
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  console.log('Created the Puppeteer Page.');
+  await page.setUserAgent(PAGE_CONFIG.USER_AGENT);
+  await page.setViewport({
+    width: PAGE_CONFIG.WIDTH,
+    height: PAGE_CONFIG.HEIGHT
+  });
+
+  // 2 if cookies file exists, read cookies
+  const cookiesFile = path.join(homedir, CONFIG.OUTPUT_DIR) + '/cookies.json';
+  const previousSession = fs.existsSync(cookiesFile);
+  if (previousSession) {
+    const content = fs.readFileSync(cookiesFile);
+    const cookies = JSON.parse(content);
+    if (cookies.length !== 0) {
+      for (const cookie of cookies) {
+        await page.setCookie(cookie);
+      }
+      console.log('Cookies have been loaded in the browser.');
+    }
+  }
+
+  // 3 go to start
+  await page.goto(CONFIG.START_URL);
+  await page.addScriptTag({
+    url: 'https://cdn.jsdelivr.net/npm/lodash@4/lodash.min.js'
+  });
+
+  // 4 log in, if cookies did not
+  const isLogInForm = await page.$(SELECTORS.USERNAME_SELECTOR);
+  const isThePage = await page.$(SELECTORS.PRODTYPE_SELECTOR);
+
+  if (isLogInForm) {
+    await page.type(SELECTORS.USERNAME_SELECTOR, CREDENTIALS.USER);
+    await page.type(SELECTORS.PASSWORD_SELECTOR, CREDENTIALS.PASS);
+    await page.click(SELECTORS.LOGIN_BUTTON_SELECTOR);
+    await page.waitForNavigation();
+    console.log('Logged in to Production Listings.');
+  } else if (isThePage) {
+    console.log('Cookies got us to Production Listings.');
+  } else {
+    console.log(error('WTF?'));
+  }
+
+  return {
+    browser,
+    cookiesFile,
+    page,
+    timeStart
+  };
+};
+
+const beforeEach = async ({ page, msg }) => {
+  if (msg) {
+    console.log(verbose(msg));
+  }
+  if (page.url !== CONFIG.START_URL) {
+    await page.goto(CONFIG.START_URL);
+    await page.addScriptTag({
+      url: 'https://cdn.jsdelivr.net/npm/lodash@4/lodash.min.js'
+    });
+  }
+};
+
+const afterEach = ({ msg }) => {
+  if (msg) {
+    console.log(success(msg));
+  }
+};
+
+const after = async ({
+  browser,
+  cookiesFile,
+  page,
+  timeStart
+}) => {
+  // save session cookies
+  const cookies = await page.cookies();
+  cookies.sort((a, b) => a.name.localeCompare(b.name));
+  fs.writeFile(cookiesFile, JSON.stringify(cookies, null, 2), (err) => {
+    if (err) throw err;
+    console.log('Cookies have been saved to', cookiesFile);
+  });
+
+  await page.waitForTimeout(1500);
+  await browser.close();
+  const timeStop = new Date();
+  console.log('Stop time:', timeStop.toLocaleString());
+  const duration = Math.floor((timeStop - timeStart) / 1000);
+  console.log(`Program took ${duration} seconds.`);
+};
+
+module.exports = {
+  before,
+  beforeEach,
+  afterEach,
+  after
+};
--- src/location.js
+++ src/location.js
@@ -5,7 +5,6 @@ const pull = require('lodash/pull');
 const remove = require('lodash/remove');
 const homedir = require('os').homedir();
 const path = require('path');
-const puppeteer = require('puppeteer');
 
 const chalk = require('chalk');
 const error = chalk.bold.red;
@@ -14,8 +13,6 @@ const verbose = chalk.bold.yellow;
 
 const CONFIG = require('./config.js');
 const CONSTANTS = require('./constants.js');
-const CREDENTIALS = require('./credentials.js');
-const PAGE_CONFIG = require('./page-config.js');
 const SELECTORS = require('./selectors.js');
 
 const PRODTYPE_ALL = CONSTANTS.PRODTYPES[0].value;
@@ -24,101 +21,18 @@ const PRODTYPE_ALL = CONSTANTS.PRODTYPES[0].value;
 const LOCATIONS = ['LA'];
 
 const handlers = require('./handlers.js');
+const hooks = require('./hooks.js');
 
 (async () => {
-  /* ***************************** */
-  let browser;
-  let cookiesFile;
-  let page;
-  let timeStart;
-
-  const before = async () => {
-    // 1 setup
-    timeStart = new Date();
-    console.log('Start time:', timeStart.toLocaleString());
-
-    browser = await puppeteer.launch();
-    page = await browser.newPage();
-    console.log('Created the Puppeteer Page.');
-    await page.setUserAgent(PAGE_CONFIG.USER_AGENT);
-    await page.setViewport({
-      width: PAGE_CONFIG.WIDTH,
-      height: PAGE_CONFIG.HEIGHT
-    });
-
-    // 2 if cookies file exists, read cookies
-    cookiesFile = path.join(homedir, CONFIG.OUTPUT_DIR) + '/cookies.json';
-    const previousSession = fs.existsSync(cookiesFile);
-    if (previousSession) {
-      const content = fs.readFileSync(cookiesFile);
-      const cookies = JSON.parse(content);
-      if (cookies.length !== 0) {
-        for (const cookie of cookies) {
-          await page.setCookie(cookie);
-        }
-        console.log('Cookies have been loaded in the browser.');
-      }
-    }
-
-    // 3 go to start
-    await page.goto(CONFIG.START_URL);
-    await page.addScriptTag({
-      url: 'https://cdn.jsdelivr.net/npm/lodash@4/lodash.min.js'
-    });
-
-    // 4 log in, if cookies did not
-    const isLogInForm = await page.$(SELECTORS.USERNAME_SELECTOR);
-    const isThePage = await page.$(SELECTORS.PRODTYPE_SELECTOR);
-
-    if (isLogInForm) {
-      await page.type(SELECTORS.USERNAME_SELECTOR, CREDENTIALS.USER);
-      await page.type(SELECTORS.PASSWORD_SELECTOR, CREDENTIALS.PASS);
-      await page.click(SELECTORS.LOGIN_BUTTON_SELECTOR);
-      await page.waitForNavigation();
-      console.log('Logged in to Production Listings.');
-    } else if (isThePage) {
-      console.log('Cookies got us to Production Listings.');
-    } else {
-      console.log(error('WTF?'));
-    }
-  };
-
-  const beforeEach = async (location) => {
-    console.log(verbose(`Begin "for" loop (${location})`));
-    if (page.url !== CONFIG.START_URL) {
-      await page.goto(CONFIG.START_URL);
-      await page.addScriptTag({
-        url: 'https://cdn.jsdelivr.net/npm/lodash@4/lodash.min.js'
-      });
-    }
-  };
-
-  const afterEach = (location) => {
-    console.log(success(`End "for" loop (${location})`));
-  };
-
-  const after = async () => {
-    // save session cookies
-    const cookies = await page.cookies();
-    cookies.sort((a, b) => a.name.localeCompare(b.name));
-    fs.writeFile(cookiesFile, JSON.stringify(cookies, null, 2), (err) => {
-      if (err) throw err;
-      console.log('Cookies have been saved to', cookiesFile);
-    });
-
-    await page.waitForTimeout(1500);
-    await browser.close();
-    const timeStop = new Date();
-    console.log('Stop time:', timeStop.toLocaleString());
-    const duration = Math.floor((timeStop - timeStart) / 1000);
-    console.log(`Program took ${duration} seconds.`);
-  };
-
-  /* ***************************** */
-  await before();
+  const {
+    browser,
+    cookiesFile,
+    page,
+    timeStart
+  } = await hooks.before();
 
   for (const location of LOCATIONS) {
-    await beforeEach(location);
+    await hooks.beforeEach({ page, msg: `Begin "for" loop (${location})` });
 
     const OUTPUT_DIR = path.join(homedir, CONFIG.OUTPUT_DIR, 'location', location);
     const OUTPUT_DIR_ARCHIVE = path.join(OUTPUT_DIR, 'archive');
@@ -228,9 +142,13 @@ const handlers = require('./handlers.js');
       console.log(success(outFile, 'has been saved.'));
     });
 
-    afterEach(location);
+    hooks.afterEach(`End "for" loop (${location})`);
   }
 
-  await after();
-  /* ***************************** */
+  await hooks.after({
+    browser,
+    cookiesFile,
+    page,
+    timeStart
+  });
 })();
